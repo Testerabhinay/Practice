1615599a09daed985e2cf1df13cbc4d0a7146b29
abhinayrevuru <abhinayrevuru@gmail.com>
2016-09-08 09:01:30 +0530

    first

---
+++ stars2.java
@@ -0,0 +1,18 @@
+class stars2
+{
+public static void main(String args[])
+{
+for(int row=1;row<5;row++)
+{
+for(int space =1;space<5-row;space++)
+{
+System.out.print(" ");
+}
+for(int col=1;col<=(row*2)-1;col++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+}
